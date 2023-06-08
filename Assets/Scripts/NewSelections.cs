e14f9e5087fe84166e70b9c9982bf1c2477e0a4b
Pianja <94577886+Pianja@users.noreply.github.com>
2023-06-08 18:21:46 +1000

    yes

--- Assets/Scripts/NewSelections.cs
+++ Assets/Scripts/NewSelections.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.UI;
 using UnityEngine.AI;
+using UnityEngine.EventSystems;
 
 public class NewSelections : MonoBehaviour
 {
@@ -13,9 +14,12 @@ public class NewSelections : MonoBehaviour
     
     public List<GameObject> characters;
     public List<GameObject> selectedCharacters;
+    public string clickState;
 
     private static NewSelections _instance;
     public static NewSelections Instance {  get { return _instance; } }
+
+    bool shiftPressed;
 
     private void Awake()
     {
@@ -38,14 +42,24 @@ public class NewSelections : MonoBehaviour
             Ray ray = cam.ScreenPointToRay(Input.mousePosition);
             if (Physics.Raycast(ray, out RaycastHit hit, 100) && !selectedCharacters.Contains(hit.transform.gameObject)) 
             {
-                if (hit.transform.tag.Equals("Villagers"))
+
+                if (hit.transform.tag.Equals("Villagers") && clickState == "Select" && !IsMouseOverUI() && shiftPressed == false)
+                {
+                    selectedCharacters.Clear();
+                    selectedCharacters.Add(hit.transform.gameObject);
+                    hit.transform.gameObject.GetComponent<Outline>().enabled = true;
+                    hit.transform.gameObject.GetComponent<Outline>().OutlineColor = Color.cyan;
+                    hit.transform.gameObject.GetComponent<Outline>().OutlineWidth = 10;
+                }
+                else if (hit.transform.tag.Equals("Villagers") && clickState == "Select" && !IsMouseOverUI() && shiftPressed == true)
                 {
                     selectedCharacters.Add(hit.transform.gameObject);
                     hit.transform.gameObject.GetComponent<Outline>().enabled = true;
                     hit.transform.gameObject.GetComponent<Outline>().OutlineColor = Color.cyan;
                     hit.transform.gameObject.GetComponent<Outline>().OutlineWidth = 10;
                 }
-                else if (hit.transform.tag.Equals("HarvestableLumberJack"))
+
+                else if (hit.transform.tag.Equals("HarvestableLumberJack") && clickState == "Select" && !IsMouseOverUI())
                 {
                     for (int i = 0; i < selectedCharacters.Count; i++)
                     {
@@ -58,7 +72,8 @@ public class NewSelections : MonoBehaviour
                         }
                     }
                 }
-                else if (hit.transform.tag.Equals("Ground"))
+
+                else if (hit.transform.tag.Equals("Ground") && clickState == "Move" && !IsMouseOverUI())
                 {
                     foreach (GameObject select in selectedCharacters)
                     {
@@ -76,6 +91,41 @@ public class NewSelections : MonoBehaviour
             {
                 
             }
+        }
+    }
+    public void Select()
+    {
+        clickState = "Select";
+    }
+    public void Deselect()
+    {
+        clickState = "Deselect";
+        foreach (GameObject select in selectedCharacters)
+        {
+            if (select.GetComponent<Worker>()._workerStates != Worker.WorkerStates.Working)
+            {
+                select.transform.gameObject.GetComponent<Outline>().enabled = false;
+            }
+        }
+        selectedCharacters.Clear();
+    }
+    public void Move()
+    {
+        clickState = "Move";
+    }
+    private bool IsMouseOverUI()
+    {
+        return EventSystem.current.IsPointerOverGameObject();
+    }
+    public void MultiSelect(InputAction.CallbackContext context)
+    {
+        if (context.phase == InputActionPhase.Performed)
+        {
+            shiftPressed = true;
+        }
+        else if (context.phase == InputActionPhase.Canceled)
+        {
+            shiftPressed = false;
         }
     }
 }
